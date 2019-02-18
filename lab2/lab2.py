8157c6cadef96efea486e1657b24e6026d7141e1
Kim <kmora2b@gmail.com>
2019-02-17 21:36:28 -0700

    Draft

---
+++ lab2/lab2.py
@@ -0,0 +1,37 @@
+"""
+Course: CS 2302 [MW 1:30-2:50] 
+Author: Kimberly Morales
+Assignment: Lab 2
+Instructor: Olac Fuentes
+TA(s): Anindita Nath , Maliheh Zargaran 
+Date: 
+Date of last modification: 
+
+Purpose of program: 
+
+"""
+import timeit
+
+
+#def bubble_sort():
+
+#def merge_sort():
+
+#def quick_sort():
+
+#def mod_quick_sort():
+
+#def median(L):
+
+
+#def gen_rand_list(n):
+
+
+
+start = timeit.default_timer()
+for i in range(10000):
+	print("AA")
+stop = timeit.default_timer()
+
+
+print('Time: ', stop - start) 
